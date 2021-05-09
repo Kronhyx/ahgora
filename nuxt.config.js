c3821d6a534023bbfcf4f82c9761561704c03197
Randy Tellez Galan <kronhyx@gmail.com>
2021-05-09 15:49:23 -0300

    chore: install nuxt svg loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,29 +4,43 @@ export default {
     title: '@kronhyx/ahgora',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: ''
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: 'https://leap.mediumra.re/assets/css/theme.min.css'
+      }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
+    ['@nuxt/typescript-build', {}], // https://go.nuxtjs.dev/typescript
+    ['@nuxtjs/svg', {}]// https://github.com/nuxt-community/svg-module
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -50,6 +64,5 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
 }
